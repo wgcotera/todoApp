c4f6ab42820cc883e7a769078046ef33ace7b337
wgcotera <wgcotera@espol.edu.ec>
2023-07-01 19:45:05 -0500

    fix(backEnd): cors

--- backEnd/repository/todo_repository.rb
+++ backEnd/repository/todo_repository.rb
@@ -9,8 +9,8 @@ class TodoRepository
     def update(id, request_data)
         @todo = Todo.find_by(id: id)
         raise TodoNotFound unless @todo
-        @todo.content = request_data[:content] if request_data.key?[:content]
-        @todo.status = request_data[:status] if request_data.key?[:status]
+        @todo.content = request_data[:content] if request_data.key?(:content)
+        @todo.status = request_data[:status] if request_data.key?(:status)
         raise TodoNotUpdated unless @todo.save
         @todo
     end
--- backEnd/server.rb
+++ backEnd/server.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/json'
 require 'json'
+require 'sinatra/cross_origin'
 
 require './models/todo'
 require_relative './repository/todo_repository'
@@ -10,6 +11,25 @@ require_relative 'exceptions/todo_exceptions'
 set :database, {adapter: 'sqlite3', database: 'todos.db'}
 
 todo_repository = TodoRepository.new
+
+configure do
+    enable :cross_origin
+end
+
+before do
+    content_type :json
+    headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
+            'Access-Control-Allow-Headers' => 'Content-Type'
+end
+
+
+options "*" do
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
+end
 
 get '/' do
     @todos = Todo.all
@@ -21,9 +41,9 @@ post '/todos' do
     result = todo_repository.create(request_data)
     status 201
     json result
-    rescue TodoNotSaved => e
-        status 400
-        json e.message
+rescue TodoNotSaved => e
+    status 400
+    json e.message
 end
 
 put '/todos/:id' do
@@ -31,22 +51,22 @@ put '/todos/:id' do
     result = todo_repository.update(params[:id], request_data)
     status 200
     json result
-    rescue TodoNotFound => e
-        status 404
-        json e.message
-    rescue TodoNotUpdated => e
-        status 400
-        json e.message
+rescue TodoNotFound => e
+    status 404
+    json e.message
+rescue TodoNotUpdated => e
+    status 400
+    json e.message
 end
 
 delete '/todos/:id' do
     result = todo_repository.delete(params[:id])
     status 200
     json result
-    rescue TodoNotFound => e
-        status 404
-        json e.message
-    rescue TodoNotDeleted => e
-        status 400
-        json e.message
+rescue TodoNotFound => e
+    status 404
+    json e.message
+rescue TodoNotDeleted => e
+    status 400
+    json e.message
 end
